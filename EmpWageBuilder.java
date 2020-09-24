225fc67dbdadaee3636078d59426b1d21bcf1bef
lkvreddy007 <lkvreddy007@gmail.com>
2020-09-24 11:19:30 +0530

    [Krishna]added-usecase2

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,6 +5,7 @@ class EmpWageBuilder {
 		System.out.println("Helloworld");
 	}
 }
+//usecase1
 class empcheck {
 	public static void main(String[] args) {
 		int isfulltime=1;
@@ -17,4 +18,23 @@ class empcheck {
 		}
 	}
 
+}
+//usecase2
+class EmpWageBuilderUC2 {
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		int empHrs=0;
+		int empWage=0;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck== IS_FULL_TIME) {
+			empHrs=8;
+		}
+		else {
+			empHrs=0;
+		}
+		empWage= empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage for "+i+"th employee "+empWage);
+	}}
 }
