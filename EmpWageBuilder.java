a532ebbc8345f3b667506b9372c122ad728ade81
lkvreddy007 <lkvreddy007@gmail.com>
2020-09-24 11:12:36 +0530

    [Krishna]added-usecase1

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,7 +1,20 @@
 package WageBuilder;
 
-public class EmpWageBuilder {
+class EmpWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Helloworld");
 	}
+}
+class empcheck {
+	public static void main(String[] args) {
+		int isfulltime=1;
+		double empcheck= Math.floor(Math.random()*10)%2; 
+		if(empcheck==isfulltime) {
+			System.out.println("Employee present");
+		}
+		else {
+			System.out.println("Employee absent");
+		}
+	}
+
 }
